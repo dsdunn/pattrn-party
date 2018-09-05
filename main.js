a01972c89158f55e233f568fe3d2186210ed9124
dsdunn <dsdunnmusic@gmail.com>
2018-09-04 20:07:38 -0600

    Finish responsive menu and sandbox

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ document.querySelector('.accordion-tabs .view-code').addEventListener('click', f
   document.querySelector('.sandbox-accordion').classList.toggle('hidden');
 })
 
+document.querySelector('.cards .view-code').addEventListener('click', function() {
+  document.querySelector('.sandbox-cards').classList.toggle('hidden');
+})
+
+document.querySelector('.header-bar-section .view-code').addEventListener('click', function() {
+  document.querySelector('.sandbox-menu').classList.toggle('hidden');
+})
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+  document.querySelector('.mobile-menu').classList.toggle('visible');
+})
+
+
